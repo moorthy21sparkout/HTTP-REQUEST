470ec55f816ecc3b6766c3d0e3027e84e5704c3e
moorthy21sparkout <moorthy@sparkouttech.com>
2024-08-29 11:37:58 +0530

    Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {path: 'home',component:HomeComponent},
+  {path:'Contact-us',component:ContactusComponent},
+  {path: 'about-us',component:AboutComponent}
+];
